346be524855c9657eeb4a37ff4196118d4b7c904
Rene Olsen <wwwrene@runbox.com>
2016-03-07 13:18:30 +0100

    Init permutaion

---
+++ int.go
@@ -0,0 +1,42 @@
+package int
+
+func Perm2(n int, eval func([2]int) bool) [2]int {
+	for i := 0; i < n-1; i++ {
+		for j := i + 1; j < n; j++ {
+			v := [2]int{i, j}
+			if eval(v) {
+				return v
+			}
+		}
+	}
+	return [2]int{-1, -1}
+}
+
+func Perm3(n int, eval func([3]int) bool) [3]int {
+	for i0 := 0; i0 < n-2; i0++ {
+		for i1 := i0 + 1; i1 < n-1; i1++ {
+			for i2 := i1 + 1; i2 < n; i2++ {
+				v := [3]int{i0, i1, i2}
+				if eval(v) {
+					return v
+				}
+			}
+		}
+	}
+	return [3]int{-1, -1, -1}
+}
+func Perm4(n int, eval func([4]int) bool) [4]int {
+	for i0 := 0; i0 < n-3; i0++ {
+		for i1 := i0 + 1; i1 < n-2; i1++ {
+			for i2 := i1 + 1; i2 < n-1; i2++ {
+				for i3 := i2 + 1; i3 < n; i3++ {
+					v := [4]int{i0, i1, i2, i3}
+					if eval(v) {
+						return v
+					}
+				}
+			}
+		}
+	}
+	return [4]int{-1, -1, -1, -1}
+}
---
+++ int_test.go
@@ -0,0 +1,76 @@
+package int
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPerm2(t *testing.T) {
+	counter := 0
+	n := 60
+	unique := make(map[string][2]int)
+	result := [2]int{-1, -1}
+	v := Perm2(n, func(v [2]int) bool {
+		t.Logf("%v", v)
+		counter++
+		unique[fmt.Sprintf("%v", v)] = v
+		return false
+	})
+	per := n * (n - 1) / 2
+	if counter != per {
+		t.Errorf("Counter is wrong %v is not %v", counter, per)
+	}
+	if v != result {
+		t.Errorf("Result is wrong expect -1,-1 got %v", v)
+	}
+	if len(unique) != per {
+		t.Errorf("Unique is wrong expect %v got %v", per, unique)
+	}
+
+}
+func TestPerm4(t *testing.T) {
+	counter := 0
+	n := 60
+	unique := make(map[string][4]int)
+	result := [4]int{-1, -1, -1, -1}
+	v := Perm4(n, func(v [4]int) bool {
+		t.Logf("%v", v)
+		counter++
+		unique[fmt.Sprintf("%v", v)] = v
+		return false
+	})
+	per := n * (n - 1) * (n - 2) * (n - 3) / (4 * 3 * 2)
+	if counter != per {
+		t.Errorf("Counter is wrong expected %v got %v", per, counter)
+	}
+	if v != result {
+		t.Errorf("Result is wrong expect -1,-1,-1,-1 got %v", v)
+	}
+	if len(unique) != per {
+		t.Errorf("Unique is wrong expect %v got %v", per, len(unique))
+	}
+
+}
+func TestPerm3(t *testing.T) {
+	counter := 0
+	n := 60
+	unique := make(map[string][3]int)
+	result := [3]int{-1, -1, -1}
+	v := Perm3(n, func(v [3]int) bool {
+		t.Logf("%v", v)
+		counter++
+		unique[fmt.Sprintf("%v", v)] = v
+		return false
+	})
+	per := n * (n - 1) * (n - 2) / (3 * 2)
+	if counter != per {
+		t.Errorf("Something is wrong expected %v got %v", per, counter)
+	}
+	if v != result {
+		t.Errorf("Something is wrong expect -1,-1,-1 got %v", v)
+	}
+	if len(unique) != per {
+		t.Errorf("Something is wrong expect %v got %v", per, len(unique))
+	}
+
+}
